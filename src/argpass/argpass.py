b8f30d749095bd1a4281250e92411e213f1f4788
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2022-04-22 16:14:00 +0200

    implement different approach with two parser instances

--- src/argpass/argpass.py
+++ src/argpass/argpass.py
@@ -11,8 +11,8 @@ class NargsOption(Enum):
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.special_args: dict[str, tuple[str, str]] = {}
-        self.dummy_prefix_char: str = "?" if self.prefix_chars != "?" else "-"
+        self.dummy_args: dict[str, str] = {}
+        self.dummy_prefix_char: str = "?" if "?" not in self.prefix_chars else "-"
 
     def add_argument(
         self,
@@ -24,13 +24,9 @@ class ArgumentParser(argparse.ArgumentParser):
             and kwargs["nargs"] == NargsOption.COLLECT_UNTIL_NEXT_KNOWN
         ):
             for arg in name_or_flags:
-                dest = (
-                    arg.lstrip("-").replace("-", "_")
-                    if "dest" not in kwargs
-                    else kwargs["dest"]
-                )
-                self.special_args[arg] = ("--dummy" + arg, dest)
-            kwargs["nargs"] = argparse.ZERO_OR_MORE
+                dummy_arg = f"?dummy{arg}"
+                self.dummy_args[arg] = dummy_arg
+            kwargs["nargs"] = 1
         return super().add_argument(*name_or_flags, **kwargs)
 
     def parse_known_args(  # type: ignore
@@ -42,25 +38,21 @@ class ArgumentParser(argparse.ArgumentParser):
         manipulated_args: list[str] = []
         for arg in args:
             manipulated_args.append(arg)
-            if arg in self.special_args:
-                manipulated_args.append(self.special_args[arg][0])
+            if arg in self.dummy_args:
+                manipulated_args.append(self.dummy_args[arg])
+                manipulated_args.append(self.dummy_args[arg])
         parsed_args, unknown = super().parse_known_args(manipulated_args, namespace)
-        active_special_arg_dest = None
-        still_unknown = []
-        dummy_args = {
-            dummy_arg: (arg, dest)
-            for arg, (dummy_arg, dest) in self.special_args.items()
-        }
-        for arg in unknown:
-            if arg in dummy_args:
-                active_special_arg_dest = dummy_args[arg][1]
-                special_arg_list = vars(parsed_args)[active_special_arg_dest]
-            else:
-                if active_special_arg_dest is None:
-                    # unknown argument occurring before the first special argument
-                    # --> remains unknown
-                    still_unknown.append(arg)
-                    continue
-                special_arg_list.append(arg)
-        unknown = [arg for arg in unknown if not arg.startswith("--dummy")]
+        # parse again using the dummy prefix
+        dummy_parser = argparse.ArgumentParser(prefix_chars=self.dummy_prefix_char)
+        for arg, dummy_arg in self.dummy_args.items():
+            dummy_parser.add_argument(dummy_arg, dest=dummy_arg, nargs="+")
+        parsed_dummy_args, still_unknown = dummy_parser.parse_known_args(unknown)
+        # replace the dummy args in the originally parsed arguments
+        for dest, arg in vars(parsed_args).items():
+            if (
+                isinstance(arg, list)
+                and len(arg) == 1
+                and arg[0] in self.dummy_args.values()
+            ):
+                vars(parsed_args)[dest] = vars(parsed_dummy_args)[arg[0]]
         return parsed_args, still_unknown
